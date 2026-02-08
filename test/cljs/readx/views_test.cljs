9001a5602298b46df1a6c0243c2f7b06cd88cca7
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2026-02-08 22:53:48 +0000

    Add CLJS tests for to-bionic function covering Latin, Cyrillic, Greek, and edge cases

--- test/cljs/readx/views_test.cljs
+++ test/cljs/readx/views_test.cljs
@@ -1,14 +1,47 @@
 (ns readx.views-test
-  (:require [cljs.test :refer [deftest is]]
-            [clojure.string :as str]
+  (:require [cljs.test :refer [deftest is testing]]
             [readx.views :as views]))
+
+(def ^:private to-bionic #'views/to-bionic)
 
 (deftest test-home-view-returns-hiccup
   (let [result (views/home-view {})]
     (is (vector? result))
     (is (= :div (first result)))))
 
-(deftest test-home-view-contains-heading
-  (let [result (views/home-view {})
-        html-str (str result)]
-    (is (str/includes? html-str "Welcome"))))
+(deftest test-to-bionic-single-word
+  (is (= [:<> [:<> [:b "Hel"] "lo"]]
+         (to-bionic "Hello"))))
+
+(deftest test-to-bionic-short-words
+  (testing "one letter word"
+    (is (= [:<> [:<> [:b "I"] ""]]
+           (to-bionic "I"))))
+  (testing "two letter word"
+    (is (= [:<> [:<> [:b "i"] "s"]]
+           (to-bionic "is")))))
+
+(deftest test-to-bionic-sentence
+  (let [result (to-bionic "Hello world")]
+    (is (= [:<> [:<> [:b "Hel"] "lo"] " " [:<> [:b "wor"] "ld"]]
+           result))))
+
+(deftest test-to-bionic-preserves-punctuation
+  (is (= [:<> [:<> [:b "Hel"] "lo"] ", " [:<> [:b "wor"] "ld"] "!"]
+         (to-bionic "Hello, world!"))))
+
+(deftest test-to-bionic-cyrillic
+  (is (= [:<> [:<> [:b "Чте"] "ние"] " " [:<> [:b "эт"] "о"]]
+         (to-bionic "Чтение это"))))
+
+(deftest test-to-bionic-greek
+  (is (= [:<> [:<> [:b "Γε"] "ιά"] " " [:<> [:b "σο"] "υ"]]
+         (to-bionic "Γειά σου"))))
+
+(deftest test-to-bionic-mixed-alphabets
+  (let [result (to-bionic "Hello Мир")]
+    (is (= [:<> [:<> [:b "Hel"] "lo"] " " [:<> [:b "Ми"] "р"]]
+           result))))
+
+(deftest test-to-bionic-empty-string
+  (is (= [:<>] (to-bionic ""))))
